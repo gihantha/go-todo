265021adf8964b00ca8024998a327642223f3e4a
Kaveen <gihantha01@gmail.com>
2025-08-06 10:39:53 +0530

    loops in go used

--- main.go
+++ main.go
@@ -7,32 +7,43 @@ func main() {
 	var taskTwo = "Clean the house"
 	reward := "Reward yourself with a treat after completing tasks!"
 
-	var taskItems = []string{"Watch a movie", "Read a book", "Go for a walk"}
+	taskItems := []string{taskOne, taskTwo, reward}
 	
 	fmt.Println("Welcome to our Todolist App!")
 
-	fmt.Println()
-	fmt.Println("This is a simple app to manage your tasks.")
-	fmt.Println("You can add, remove, and view tasks.")
-	fmt.Println("Let's get started!")
+	// fmt.Println()
+	// fmt.Println("This is a simple app to manage your tasks.")
+	// fmt.Println("You can add, remove, and view tasks.")
+	// fmt.Println("Let's get started!")
 
-	fmt.Println()
-	fmt.Println("List of tasks:")
+	// fmt.Println()
+	// fmt.Println("List of tasks:")
 	
-	fmt.Println()
-	fmt.Println("Shopping tasks:")
-	fmt.Println("1. " + taskOne)
-	fmt.Println("2. " + reward)
+	// fmt.Println()
+	// fmt.Println("Shopping tasks:")
+	// fmt.Println("1. " + taskOne)
+	// fmt.Println("2. " + reward)
 
-	fmt.Println()
-	fmt.Println("Household tasks:")
-	fmt.Println("1. " + taskTwo)
-	fmt.Println("2. " + reward)
+	// fmt.Println()
+	// fmt.Println("Household tasks:")
+	// fmt.Println("1. " + taskTwo)
+	// fmt.Println("2. " + reward)
 
-	fmt.Println()
+	// fmt.Println()
 	fmt.Println("Thank you for using our Todolist App!")
 
 	fmt.Println()
+
+
+	// for _, task := range taskItems {
+	// 	fmt.Println(task)
+	// }
+
+	for index, task := range taskItems {
+		// fmt.Println(index+1, "." , task)
+		fmt.Printf("Task %d: %s\n", index+1, task)
+	}
+	// fmt.Println(taskItems)
 
 	
 }
