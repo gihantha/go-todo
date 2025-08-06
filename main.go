bce08129ebd6d5b87a5e68e5d8fcd0c300bf7e82
Kaveen <gihantha01@gmail.com>
2025-08-06 09:46:26 +0530

    variables in go used

--- main.go
+++ main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello, World!\n")
+	var taskOne = "Buy groceries"
+	var taskTwo = "Clean the house"
+	reward := "Reward yourself with a treat after completing tasks!"
+	
+	fmt.Println("Welcome to our Todolist App!")
+
+	fmt.Println()
+	fmt.Println("This is a simple app to manage your tasks.")
+	fmt.Println("You can add, remove, and view tasks.")
+	fmt.Println("Let's get started!")
+
+	fmt.Println()
+	fmt.Println("List of tasks:")
+	
+	fmt.Println()
+	fmt.Println("Shopping tasks:")
+	fmt.Println("1. " + taskOne)
+	fmt.Println("2. " + reward)
+
+	fmt.Println()
+	fmt.Println("Household tasks:")
+	fmt.Println("1. " + taskTwo)
+	fmt.Println("2. " + reward)
+
+	fmt.Println()
+	fmt.Println("Thank you for using our Todolist App!")
+
+	
 }
