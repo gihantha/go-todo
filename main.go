5ff4b68d1232227a570ea5c7639be3bb12727f0d
Kaveen <gihantha01@gmail.com>
2025-08-06 17:27:04 +0530

    functions and variables scopes in go used

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import "fmt"
 
-func main() {
-	var taskOne = "Buy groceries"
-	var taskTwo = "Clean the house"
-	reward := "Reward yourself with a treat after completing tasks!"
 
-	taskItems := []string{taskOne, taskTwo, reward}
+
+
+func main() {
+	
 	
 	fmt.Println("Welcome to our Todolist App!")
+
+	var taskOne = "Buy groceries"
+	var taskTwo = "Clean the house"
+	var reward = "Reward yourself with a treat after completing tasks!"
+
+	var taskItems = []string{taskOne, taskTwo, reward}
 
 	// fmt.Println()
 	// fmt.Println("This is a simple app to manage your tasks.")
@@ -30,20 +35,45 @@ func main() {
 	// fmt.Println("2. " + reward)
 
 	// fmt.Println()
-	fmt.Println("Thank you for using our Todolist App!")
+	// fmt.Println("Thank you for using our Todolist App!")
 
-	fmt.Println()
+	// fmt.Println()
 
 
 	// for _, task := range taskItems {
 	// 	fmt.Println(task)
 	// }
 
-	for index, task := range taskItems {
-		// fmt.Println(index+1, "." , task)
-		fmt.Printf("Task %d: %s\n", index+1, task)
-	}
+	// for index, task := range taskItems {
+	// 	// fmt.Println(index+1, "." , task)
+	// 	fmt.Printf("Task %d: %s\n", index+1, task)
+	// }
 	// fmt.Println(taskItems)
 
+	printTasks(taskItems)
+	  fmt.Println()
+	taskItems = addTask(taskItems, "Walk the dog")
+	fmt.Println()
+
+
+	printTasks(taskItems)
+
+	
+}
+
+func printTasks(taskItems []string) {
+	fmt.Println("Thank you for using our Todolist App!")
+
+	fmt.Println()
+
+	for index, task := range taskItems {
+		fmt.Println("hb")
+		fmt.Printf("Task %d: %s\n", index+1, task)
+	} 
+}
+
+func addTask(taskItems []string, newTask string) []string{
+	var updatedTaskItems  =  append(taskItems, newTask)
+	return updatedTaskItems
 	
 }
